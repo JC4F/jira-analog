d39166c30e7547c5032e722c05c9914b1a685683
jc4f <lequan12122002@gmail.com>
2024-07-29 16:36:10 +0700

    change assest to api route

---
+++ src/server/routes/v1/project.ts
@@ -0,0 +1,4 @@
+import { defineEventHandler } from 'h3';
+import * as data from '../../../assets/data/project.json';
+
+export default defineEventHandler(() => data);
---
+++ src/server/routes/v1/user.ts
@@ -0,0 +1,4 @@
+import { defineEventHandler } from 'h3';
+import * as data from '../../../assets/data/auth.json';
+
+export default defineEventHandler(() => data);
