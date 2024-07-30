1546bd4198eb43733eb4ff44986de0ad34a7f4c1
jc4f <lequan12122002@gmail.com>
2024-07-30 10:37:47 +0700

    fix jit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import 'zone.js';
+import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
